ebf6913fff594c6c039113c2ff588f9f235deb44
Isaac Stofko <isaacstofko@gmail.com>
2024-07-30 22:46:39 -0400

    css

--- navbar.js
+++ navbar.js
@@ -6,162 +6,221 @@ class Navbar extends HTMLElement {
 
   connectedCallback() {
     this.shadowRoot.innerHTML = `
-            <style>
-         
-            #nav-container {    
-                display: flex; 
-                flex-direction: column;
-                width: 100vw; 
-                position: fixed;
-                top: 0;
-                z-index: 1000;
-                box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
-
-            }
-            .logoWrap {
-                display: flex; 
-                justify-content: center; 
-              }
-              .logoWrap img {
-                width: 300px; 
-              }
-              .logo-wrap-small img {
-                width: 140px; 
-              }
-              
-              .homepage-wrap {
-                height: 100%; 
-              }
-              
-              .header {
-                position: relative; 
-                background-color: #ffffff;
-                padding: 10px 5%;
-               
-              }
-              
-              .header-link{
-                display: flex; 
-                justify-content: space-between; 
-              }
-              .header a.main {
-                text-decoration: none; 
-                font-size: 25px; 
-                font-weight: 600; 
-                color: #000000; 
-                font-family: sans-serif;  
-                background-color: #ffffff ;
-                display: flex; 
-                align-items: flex-end; 
-                padding-left: 10px; 
-                cursor: pointer;
-              }
-            
-              .header a.main:hover {
-              color: rgb(139, 2, 243)
-              }
-                /* Nav Styles  */
-                ul {
-                    list-style-type: none; 
-                    padding: 5px; 
-                    background-color: white; 
-                }
-                .nav {
-                    display: flex; 
-                    justify-content: flex-end; 
-                    box-shadow: inset 0 0.2px 0 black;
-                    justify-content: flex-end;
-                    align-items: center;
-                    background-color: #ffffff;
-                }
-                ul.nav.nav-tabs {
-                    margin: 0; 
-                }
-                .nav-item {
-                    padding-right: 50px; 
-                }
-                .nav-item a {
-                    color: #000000; 
-                    text-decoration: none;
-                    padding: 0px 20px; 
-                    font-size: 20px; 
-                    border-radius: 2px; 
-                    background-color: #ffffff; 
-                    z-index: 2000; 
-                    cursor: pointer; 
-                    font-family: "Roboto Mono", monospace;
-                }
-                .nav-item.login button {
-                    border: none; 
-                    color: #ffffff;
-                    background-color: #000000;
-                    padding: 5px 10px; 
-                    border-radius: 2px; 
-                    cursor: pointer;
-                    font-size: 19px; 
-                    font-family: "Roboto Mono", monospace;
-                }
-                .nav-item.login button:hover, .nav-item.book button:hover, .nav-item a:hover {
-                    color: rgb(139, 2, 243);
-                    font-weight: 600; 
-                }
-                .nav-item.book a{
-                    border: none; 
-                    font: sans-serif; 
-                   
-                   
-                    font-size: 20px; 
-                    cursor: pointer;
-                    font-family: "Roboto Mono", monospace;
-                }
-           
-            </style>
-          
-            <div id="nav-container">
-            
-            <div class="header">
-            <div class="header-link">
-             <a href="/" class="main">Thomas Cook Hair</a>
-             <div class="logo-container header">
-              </div>
-              <div class="logo-wrap-small">
-              <a href="/"> <img src="/assets/pictures/thincleartwo.png" alt="thomas cook hair logo" /></a> 
-                 </div>
-              </div>
-          </div>
-          <nav id="nav"> 
-          <ul class="nav nav-tabs">
-              <li class="nav-item">
-                  <a href="/about.html">About</a>
-              </li>
-              <li class="nav-item">
-                  <a href="/work.html">Work</a> 
-              </li>
-              <li class="nav-item login">
-                  <button class="login">Sign in</button>
-              </li> 
-              <li class="nav-item book">
-                  <a href="/book.html">Book</a>
-              </li>  
-          </ul> 
-      </nav>
+      <style>
+        #nav-container {    
+          display: flex; 
+          flex-direction: column;
+          width: 100vw; 
+          position: fixed;
+          top: 0;
+          z-index: 1000;
+          box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1);
+        }
+        .logoWrap {
+          display: flex; 
+          justify-content: center; 
+        }
+        .logoWrap img {
+          width: 300px; 
+        }
+        .logo-wrap-small img {
+          width: 200px; 
+        }
+        .homepage-wrap {
+          height: 100%; 
+        }
+        .header {
+          position: relative; 
+          background-color: #ffffff;
+          padding: 10px 5%;
+        }
+        .header-link{
+          display: flex; 
+          justify-content: space-between; 
+        }
+        .header a.main {
+          text-decoration: none; 
+          font-size: 25px; 
+          font-weight: 600; 
+          color: #000000; 
+          font-family: sans-serif;  
+          background-color: #ffffff ;
+          display: flex; 
+          align-items: flex-end; 
+          padding-left: 10px; 
+          cursor: pointer;
+        }
+        .header a.main:hover {
+          color: rgb(139, 2, 243);
+        }
+        ul {
+          list-style-type: none; 
+          padding: 5px; 
+          background-color: white; 
+        }
+        .nav {
+          display: flex; 
+          justify-content: flex-end; 
+          box-shadow: inset 0 0.2px 0 black;
+          justify-content: flex-end;
+          align-items: center;
+          background-color: #ffffff;
+        }
+        ul.nav.nav-tabs {
+          margin: 0; 
+        }
+        .nav-item {
+          padding-right: 50px; 
+        }
+        .nav-item a {
+          color: #000000; 
+          text-decoration: none;
+          padding: 0px 20px; 
+          font-size: 20px; 
+          border-radius: 2px; 
+          background-color: #ffffff; 
+          z-index: 2000; 
+          cursor: pointer; 
+          font-family: "Roboto Mono", monospace;
+        }
+        .nav-item.login button {
+          border: none; 
+          color: #ffffff;
+          background-color: #000000;
+          padding: 5px 10px; 
+          border-radius: 2px; 
+          cursor: pointer;
+          font-size: 19px; 
+          font-family: "Roboto Mono", monospace;
+        }
+        .nav-item.login button:hover, .nav-item.book button:hover, .nav-item a:hover {
+          color: rgb(139, 2, 243);
+          font-weight: 600; 
+        }
+        .nav-item.book a {
+          border: none; 
+          font: sans-serif; 
+          font-size: 20px; 
+          cursor: pointer;
+          font-family: "Roboto Mono", monospace;
+        }
+        .modal {
+          display: none; 
+          position: fixed; 
+          z-index: 1001; 
+          left: 0;
+          top: 0;
+          width: 100%;
+          height: 100%;
+          overflow: auto;
+          background-color: rgba(0,0,0,0.4); 
+        }
+        .modal-content {
+          background-color: #fefefe;
+          margin: 15% auto; 
+          padding: 20px;
+          border: 1px solid #888;
+          width: 80%; 
+          max-width: 500px;
+          border-radius: 5px;
+          position: relative;
+        }
+        .close {
+          color: #aaa;
+          float: right;
+          font-size: 28px;
+          font-weight: bold;
+          cursor: pointer;
+        }
+        .close:hover,
+        .close:focus {
+          color: black;
+          text-decoration: none;
+          cursor: pointer;
+        }
+      </style>
+      <script src="https://apis.google.com/js/platform.js" async defer></script>
+      <script src="https://accounts.google.com/gsi/client" async></script>
+      <div id="nav-container">
+        <div class="header">
+          <div class="header-link">
+            <a href="/" class="main">Thomas Cook Hair</a>
+            <div class="logo-container header"></div>
+            <div class="logo-wrap-small">
+              <a href="/"><img src="/assets/pictures/thincleartwo.png" alt="thomas cook hair logo" /></a> 
             </div>
-          
-         
-        `;
+          </div>
+        </div>
+        <nav id="nav"> 
+          <ul class="nav nav-tabs">
+            <li class="nav-item">
+              <a href="/about.html">About</a>
+            </li>
+            <li class="nav-item">
+              <a href="/work.html">Work</a> 
+            </li>
+            <li class="nav-item login">
+              <button class="login">Sign in</button>
+            </li> 
+            <li class="nav-item book">
+              <a href="/book.html">Book</a>
+            </li>  
+          </ul> 
+        </nav>
+      </div>
+      <!--Popup Modal -->
+      <div id="loginModal" class="modal">
+        <div class="modal-content">
+          <div class="modal-header">
+            <span class="close">&times;</span>
+            <h2>Sign in</h2>
+          </div>
+          <div id="g_id_onload"
+               data-client_id="378842120365-n7r25rll2sqjhc582776d4ebdjp4al5o.apps.googleusercontent.com"
+               data-context="signin"
+               data-ux_mode="popup"
+               data-login_uri="http://localhost:5500"
+               data-auto_prompt="false">
+          </div>
+          <div class="g_id_signin"
+               data-type="standard"
+               data-shape="rectangular"
+               data-theme="outline"
+               data-text="signin_with"
+               data-size="large"
+               data-logo_alignment="left">
+          </div>
+        </div>
+      </div>
+    `;
 
     // Add event listener for the sign-in button
     this.shadowRoot
       .querySelector(".login button")
       .addEventListener("click", () => {
-        // Custom logic for the sign-in button
-        console.log("Sign-in button clicked");
-        // Example: Open the login modal
-        const loginModal = document.getElementById("loginModal");
+        const loginModal = this.shadowRoot.getElementById("loginModal");
         if (loginModal) {
           loginModal.style.display = "block";
         }
       });
+
+    // Add event listener for the close button in the modal
+    this.shadowRoot
+      .querySelector(".modal .close")
+      .addEventListener("click", () => {
+        const loginModal = this.shadowRoot.getElementById("loginModal");
+        if (loginModal) {
+          loginModal.style.display = "none";
+        }
+      });
+
+    // Close the modal when clicking outside of it
+    window.addEventListener("click", (event) => {
+      const loginModal = this.shadowRoot.getElementById("loginModal");
+      if (event.target === loginModal) {
+        loginModal.style.display = "none";
+      }
+    });
   }
 }
 
--- script.js
+++ script.js
@@ -1,103 +1,103 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const links = document.querySelectorAll('a[data-link]');
-    const content = document.getElementById('content');
-    const loginModal = document.getElementById('loginModal');
-    const loginButton = document.querySelector('button.login');
-    const closeButton = document.querySelector('.close');
+// document.addEventListener('DOMContentLoaded', () => {
+//     const links = document.querySelectorAll('a[data-link]');
+//     const content = document.getElementById('content');
+//     const loginModal = document.getElementById('loginModal');
+//     const loginButton = document.querySelector('button.login');
+//     const closeButton = document.querySelector('.close');
 
-    async function loadContent(page) {
-        try {
-            const response = await fetch(`${page}.html`);
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            const html = await response.text();
-            content.innerHTML = html;
-        } catch (error) {
-            console.error('Fetch error:', error);
-            content.innerHTML = '<p>Sorry, there was an error loading the page.</p>';
-        }
-    }
+//     async function loadContent(page) {
+//         try {
+//             const response = await fetch(`${page}.html`);
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             const html = await response.text();
+//             content.innerHTML = html;
+//         } catch (error) {
+//             console.error('Fetch error:', error);
+//             content.innerHTML = '<p>Sorry, there was an error loading the page.</p>';
+//         }
+//     }
 
-    // links.forEach(link => {
-    //     link.addEventListener('click', (event) => {
-    //         event.preventDefault();
-    //         const page = event.target.getAttribute('data-link');
-    //         history.pushState({ page }, "", `#${page}`);
-    //         loadContent(page);
-    //     });
-    // });
+//     // links.forEach(link => {
+//     //     link.addEventListener('click', (event) => {
+//     //         event.preventDefault();
+//     //         const page = event.target.getAttribute('data-link');
+//     //         history.pushState({ page }, "", `#${page}`);
+//     //         loadContent(page);
+//     //     });
+//     // });
 
-    window.addEventListener('popstate', (event) => {
-        if (event.state && event.state.page) {
-            loadContent(event.state.page);
-        }
-    });
+//     window.addEventListener('popstate', (event) => {
+//         if (event.state && event.state.page) {
+//             loadContent(event.state.page);
+//         }
+//     });
 
-    // // Load initial content based on URL hash or default to 'home'
-    // const initialPage = location.hash.replace('#', '') || 'home';
-    // loadContent(initialPage);
+//     // // Load initial content based on URL hash or default to 'home'
+//     // const initialPage = location.hash.replace('#', '') || 'home';
+//     // loadContent(initialPage);
 
-    // Event listener for login button to show modal
-    loginButton.addEventListener('click', () => {
-        loginModal.style.display = "block";
-        google.accounts.id.initialize({
-            client_id: "378842120365-n7r25rll2sqjhc582776d4ebdjp4al5o.apps.googleusercontent.com",
-            callback: handleCredentialResponse
-        });
-        google.accounts.id.renderButton(
-            document.querySelector('.g_id_signin'),
-            { theme: "outline", size: "large" }
-        );
-    });
+//     // Event listener for login button to show modal
+//     loginButton.addEventListener('click', () => {
+//         loginModal.style.display = "block";
+//         google.accounts.id.initialize({
+//             client_id: "378842120365-n7r25rll2sqjhc582776d4ebdjp4al5o.apps.googleusercontent.com",
+//             callback: handleCredentialResponse
+//         });
+//         google.accounts.id.renderButton(
+//             document.querySelector('.g_id_signin'),
+//             { theme: "outline", size: "large" }
+//         );
+//     });
 
-    // Event listener for close button to hide modal
-    closeButton.addEventListener('click', () => {
-        loginModal.style.display = "none";
-    });
+//     // Event listener for close button to hide modal
+//     closeButton.addEventListener('click', () => {
+//         loginModal.style.display = "none";
+//     });
 
-    // Close the modal when clicking outside of it
-    window.addEventListener('click', (event) => {
-        if (event.target === loginModal) {
-            loginModal.style.display = "none";
-        }
-    });
-});
+//     // Close the modal when clicking outside of it
+//     window.addEventListener('click', (event) => {
+//         if (event.target === loginModal) {
+//             loginModal.style.display = "none";
+//         }
+//     });
+// });
 
 
-document.addEventListener('DOMContentLoaded', () => {
-    // Get the modal
-    const modal = document.getElementById('loginModal');
+// document.addEventListener('DOMContentLoaded', () => {
+//     // Get the modal
+//     const modal = document.getElementById('loginModal');
     
-    // Get the button that opens the modal
-    const loginButton = document.querySelector('.login button');
+//     // Get the button that opens the modal
+//     const loginButton = document.querySelector('.login button');
     
-    // Get the <span> element that closes the modal
-    const span = document.getElementsByClassName('close')[0];
+//     // Get the <span> element that closes the modal
+//     const span = document.getElementsByClassName('close')[0];
     
-    // When the user clicks on the button, open the modal
-    if (loginButton) {
-        loginButton.onclick = () => {
-            modal.style.display = 'block';
-        };
-    }
+//     // When the user clicks on the button, open the modal
+//     if (loginButton) {
+//         loginButton.onclick = () => {
+//             modal.style.display = 'block';
+//         };
+//     }
     
-    // When the user clicks on <span> (x), close the modal
-    if (span) {
-        span.onclick = () => {
-            modal.style.display = 'none';
-        };
-    }
+//     // When the user clicks on <span> (x), close the modal
+//     if (span) {
+//         span.onclick = () => {
+//             modal.style.display = 'none';
+//         };
+//     }
     
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = (event) => {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    };
-});
+//     // When the user clicks anywhere outside of the modal, close it
+//     window.onclick = (event) => {
+//         if (event.target == modal) {
+//             modal.style.display = 'none';
+//         }
+//     };
+// });
 
 
-function handleCredentialResponse(response) {
-    console.log("Encoded JWT ID token: " + response.credential);
-}
+// function handleCredentialResponse(response) {
+//     console.log("Encoded JWT ID token: " + response.credential);
+// }
